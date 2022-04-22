2f41fbfec1789ce80b5595acae95e3fedb489532
leilen <leilen@naver.com>
2022-04-22 12:45:01 +0900

    add: eslint

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-import {MyPromise} from './my_promise.js';
+import { MyPromise } from './my_promise.js';
 
-const originPromiseTest = () =>{
-  return new Promise((resolve,reject) =>{
-    setTimeout(() =>{
-      resolve();
-    },1000)
-  })
-}
+const originPromiseTest = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+});
 
-const myPromiseTest = () =>{
-  return new MyPromise((resolve,reject) =>{
-    setTimeout(() =>{
-      resolve();
-    },1000)
-  })
-}
+const myPromiseTest = () => new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+});
 
-const main = () =>{
-  //originPromiseTest().then(() =>{
-    //console.log('fin')
-  //})
-  myPromiseTest().then(() =>{
-    console.log('fin')
-  }).catch( () =>{} )
-}
+const main = () => {
+  // originPromiseTest().then(() =>{
+  // console.log('fin')
+  // })
+  myPromiseTest().then(() => {
+    console.log('fin');
+  }).catch(() => {});
+};
 
 main();
-
